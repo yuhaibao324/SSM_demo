b3ae8f443cca1e97256ef1659d4c176b5810a1f7
yuqiang <2422321558@qq.com>
2019-04-18 21:05:21 +0800

    User实体类

---
+++ SpringMVC/src/main/java/testjstl/entity/User.java
@@ -0,0 +1,31 @@
+package testjstl.entity;
+
+/**
+ * 
+ * @ClassName:  User   
+ * @Description:User实体类
+ * @author: caiji
+ * @date: 2019年4月18日 下午5:08:55
+ */
+public class User {
+
+	String password;
+	String username;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+}
